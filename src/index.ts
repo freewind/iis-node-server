10f31f045e4415a618388e555dea98d9ccb26216
Freewind <nowindlee@gmail.com>
2019-12-25 00:34:36 +0800

    increase limit for large file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {FileUpload} from './typing';
-import path from 'path';
 import fs from 'fs';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.get('/', (req, res) => {
   console.log('/')
